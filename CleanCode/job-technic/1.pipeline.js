b020e6b81f2dc0a38154e0d3eade63f263502187
차성현 <cktjdgus45@naver.com>
2022-09-03 19:35:15 +0900

    클린코드 - 잡테크닉

---
+++ CleanCode/job-technic/1.pipeline.js
@@ -0,0 +1,6 @@
+let appliesToMass = states.includes('MA');
+ //잔소리 ::: 개발자 - 툴을 잘쓰는 사람. 생산성을 높혀주는. 프론트엔드의 플랫폼이나 라이브러리. api 함수들을 쭉 꽤고있는 사람들. filter,includes,map,reduce :::::
+// let appliesToMass = false;
+// for (const s of states) {
+//   if (s === 'MA') appliesToMass = true;
+// }
