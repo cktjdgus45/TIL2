6bba20fd1fa3b10b96153c8c381c62907c12324d
차성현 <cktjdgus45@naver.com>
2022-09-07 15:56:43 +0900

    세터제거하기

---
+++ CleanCode/apiRefactoring/세터제거하기.js
@@ -0,0 +1,9 @@
+class Person { //클래스가 read만되는지 write만되는지 결정.
+    get name() { }
+    // set name(value) {}  읽기만 가능하도록 세터 제거.
+  }
+  // class Person {
+  //   get name() {}
+  //   set name(value) {}
+  // }
+  
