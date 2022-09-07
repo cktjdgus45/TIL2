3dfbc7d8ffac444d8306e7b4f79aa58d1168ffb1
차성현 <cktjdgus45@naver.com>
2022-09-07 15:44:26 +0900

    객체통째로넘기기

---
+++ CleanCode/apiRefactoring/객체통째로넘기기.js
@@ -0,0 +1,44 @@
+export function temperatureAlerts(room, plan) {
+    const alerts = [];
+    if (!plan.withinRange(room.daysTempRange)) {
+      alerts.push('room temperature went outside range');
+    }
+  
+    return alerts;
+  }
+  
+  export class HeatingPlan {
+    constructor(temperatureRange) {
+      this._temperatureRange = temperatureRange;
+    }
+  
+    withinRange(range) {
+      return (
+        range.bottom >= this._temperatureRange.low && range.top <= this._temperatureRange.high
+      );
+    }
+  }
+  
+  // export function temperatureAlerts(room, plan) {
+  //   const alerts = [];
+  //   const low = room.daysTempRange.low;
+  //   const high = room.daysTempRange.high;
+  //   if (!plan.withinRange(low, high)) {
+  //     alerts.push('room temperature went outside range');
+  //   }
+  
+  //   return alerts;
+  // }
+  
+  // export class HeatingPlan {
+  //   constructor(temperatureRange) {
+  //     this._temperatureRange = temperatureRange;
+  //   }
+  
+  //   withinRange(bottom, top) {
+  //     return (
+  //       bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+  //     );
+  //   }
+  // }
+  
