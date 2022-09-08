68aaa92e88f041bf005dc3cf60ae8a2653cbdfc6
차성현 <cktjdgus45@naver.com>
2022-09-08 09:35:45 +0900

    생성자본문올리기

---
+++ CleanCode/handleInheritance/생성자본문올리기.js
@@ -0,0 +1,31 @@
+class Party {
+    constructor(name) {
+      this.#name = name;
+    }
+  }
+  
+  class Employee extends Party {
+    // #name;
+    #id;
+    #monthlyCost;
+    constructor(name, id, monthlyCost) {
+      super(name);
+      // this.#name = name;
+      this.#id = id;
+      this.#monthlyCost = monthlyCost;
+    }
+  }
+  
+  class Department extends Party {
+    // #name;
+    #staff;
+    constructor(name, staff) {
+      super(name);
+      // this.#name = name;
+      this.#staff = staff;
+    }
+  }
+  
+  const ellie = new Employee('엘리', 123, 13);
+  const department = new Department('개발부서', ellie);
+  
